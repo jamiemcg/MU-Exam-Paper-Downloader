11f394b7a623347b0fd28deeaacfcb4c6f27d428
Jamie McGowan <jamiemcgowan.dev@gmail.com>
2014-08-22 00:32:00 +0100

    Initiating Project
    Work in progress

---
+++ src/UI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Jamie on 22/08/2014.
+ */
+public class UI {
+    private JButton downloadButton;
+    private JTextField textField1;
+    private JProgressBar progressBar1;
+}
