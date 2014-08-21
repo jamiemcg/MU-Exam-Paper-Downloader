d56fcbf572b031ff8bd4b051755959a5c167273d
Jamie McGowan <jamiemcgowan.dev@gmail.com>
2014-08-22 00:59:13 +0100

    Add UI (WIP)

--- src/UI.java
+++ src/UI.java
@@ -4,7 +4,15 @@ import javax.swing.*;
  * Created by Jamie on 22/08/2014.
  */
 public class UI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NUIM Exam Paper Downloader");
+        frame.setContentPane(new UI().exam_paper_form);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     private JButton downloadButton;
+    private JPanel exam_paper_form;
     private JTextField textField1;
-    private JProgressBar progressBar1;
 }
